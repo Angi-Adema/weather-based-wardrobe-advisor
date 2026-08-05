f6ec45862ef167ec4b47485e2922d72820c9f897
Angi-Adema <dr.adema@gmail.com>
2026-08-05 10:00:48 -0600

    created user prompts for temperature, wind_speed, and weather_type. saved reference to save text as lowercase.

---
+++ ITS320_Week3_Angela.py
@@ -0,0 +1,16 @@
+# Python project accepting user input on weather conditions and displaying 
+# wardrobe suggestions based on the input.
+
+# Prompt user to enter the temperature value in Fahrenheit
+temperature = int(input("Please enter temperature in Fahrenheit (Example - 86): "))
+
+# Prompt user to enter wind speed in miles per hour
+wind_speed = int(input("Please enter wind speed in miles per hour (Example - 10): "))
+
+# Prompt user to enter the weather type (sunny, cloudy, rainy, snowy)
+weather_type = input("Please enter the weather type (sunny, cloudy, rainy, snowy): ".lower())
+
+
+# REFERENCES
+# 1. GeeksforGeeks. (2026, July 27). "String lower() Method in Python" GeeksforGeeks.
+#    https://www.geeksforgeeks.org/python/python-string-lower/
